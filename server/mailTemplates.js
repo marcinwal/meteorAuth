64d0f63a2c97138f5dcbf12ceb840922c65b0f7a
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-23 18:25:00 +0100

    final

--- server/mailTemplates.js
+++ server/mailTemplates.js
@@ -10,6 +10,6 @@ Accounts.emailTemplates.verifyEmail.text = function(user,url){
 
 Accounts.emailTemplates.verifyEmail.html = function(user,url){
   return "<h1>Welcome to Test app</h1>"
-         + "<p><strong>verify your email address go ahead and follow the link below:</p>"
+         + "<p><strong>verify your email address go ahead and follow the link below:</strong></p>"
          + url;
 };
