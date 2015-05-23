1846ead36dd7341bf6f6d1f4919b4ad51591c675
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-23 18:20:34 +0100

    working final

---
+++ collections/collections.js
@@ -0,0 +1,35 @@
+MessagesCollection = new Mongo.Collection('messages');
+MessagesCollection.allow({
+  insert: function(userId,doc){
+    return userId;
+  },
+  update: function(userId,doc){
+    return true;
+  },
+  remove: function(userId,doc){
+    return doc.recipient === userId;
+  }
+});
+
+MessagesCollection.deny({
+  insert: function (userId, doc) {
+    var fieldsInDoc = _.keys(doc);
+    var validFields = ['sender', 'recipient', 'timestamp', 'message', '_id'];
+    if (_.difference(fieldsInDoc, validFields).length > 0) {
+      console.log('additional fields found');
+      return true;
+    } else {
+      console.log('all fields good');
+      return false
+    }
+  },
+  remove: function (userId, doc) {
+    return doc.recipient !== userId;
+  }
+});
+
+Meteor.users.allow({
+  remove: function(userId,doc){
+    return doc._id === userId;
+  }
+});
--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
-Meteor.startup(function(){
-  process.env.ROOT_UTIL = "http://localhost:3000";  //where tp put correct email 
-});
+// Meteor.startup(function(){
+//   process.env.ROOT_UTIL = "http://localhost:3000";  //where tp put correct email 
+// });
 
-Accounts.onCreateUser(function(options,user){
+if(ServiceConfiguration.configurations.find({
+    service: 'facebook'
+  }).count() === 0) {
+    ServiceConfiguration.configurations.insert({
+      service: 'facebook',
+      appId: process.env.METEOR_FACEBOOK_ID,
+      secret: process.env.METEOR_FACEBOOK_SECRET,
+      loginStyle: 'popup'      
+    });
+}  
+
+Accounts.onCreateUser(function (options,user){
   if(options.profile){
     user.profile = options.profile;
   }
@@ -13,8 +24,13 @@ Accounts.onCreateUser(function(options,user){
   if (options.email){
     Meteor.setTimeout(function(){
       Accounts.sendVerificationEmail(user._id);
-
     },2*1000);
+  }
+  if (user.services.facebook){
+    user.profile.first_name = user.services.facebook.first_name;
+    user.profile.last_name = user.services.facebook.last_name;
+    user.profile.gender = user.services.facebook.gender;
   }
   return user;
 });
+
