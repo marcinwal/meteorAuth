9618bc4b28edb1ac172d62bca7b186ebc1da4977
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-23 13:35:59 +0100

    styling emails

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+Meteor.startup(function(){
+  process.env.ROOT_UTIL = "http://localhost:3000";  //where tp put correct email 
+});
+
 Accounts.onCreateUser(function(options,user){
   if(options.profile){
     user.profile = options.profile;
--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,19 @@
+Accounts.emailTemplates.siteName = "MeteorTest";
+Accounts.emailTemplates.from = "Marcin";
+Accounts.emailTemplates.verifyEmail.subject = function(user){
+  return 'Confirm your email address,' + user.username;
+};
+
+Accounts.emailTemplates.verifyEmail.text = function(user,url){
+  return "Welcome to the App" + "to verify your email follow the link" + url;
+};
+
+Accounts.emailTemplates.verifyEmail.html = function(user,url){
+  return "<h1>Welcome to Test app</h1>"
+         + "<p><strong>verify your email address go ahead and follow the link below:</p>"
+         + url;
+};
+
 Meteor.startup(function(){
   smtp = {
     username: process.env.GMAIL_LOGIN,
